e8cb4f10900ab8dcd0cbc591ab724787c4a93826
Artem <tommerg09@gmail.com>
2020-12-23 16:26:30 +0300

    feat: make data

---
+++ movie-house/src/data.js
@@ -0,0 +1,92 @@
+export const moviesData = {
+  "Октябрь": [
+    {
+      company: "Октябрь",
+      title: "Шанхайский Полдень",
+      img: "https://i.imgur.com/4rQOCkJ.jpg",
+      description: 
+        `Когда прекрасную китайскую принцессу похищают, Император отправляет на Дикий Запад 
+        троих самых верных и искусных телохранителей. Чон Ванг, естественно, не оказывается в их числе. 
+        Но тайными путями ему всё-таки удаётся попасть в воинственную делегацию 
+        и оказаться в самом сердце Америки и непредсказуемых событий.
+        Вместе со случайным напарником китайский телохранитель попадает во всевозможные переделки; 
+        между делом он помогает племени Сиу, выкуривает трубку мира и тут же оказывается на тропе войны, 
+        по которой пробегают его многочисленные враги.`,     
+      seats: [
+        [{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: true},{empty: true},{empty: false},{empty: false},{empty: false}],
+        [{empty: false},{empty: true},{empty: true},{empty: false},{empty: true},{empty: true},{empty: false},{empty: false},{empty: false},{empty: false}],
+        [{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false}],
+        [{empty: false},{empty: false},{empty: false},{empty: true},{empty: true},{empty: true},{empty: false},{empty: false},{empty: false},{empty: false}],
+        [{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: true},{empty: true},{empty: false}],
+        [{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false}],
+        [{empty: true},{empty: true},{empty: true},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false}],
+        [{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false}],
+        [{empty: true},{empty: false},{empty: true},{empty: true},{empty: true},{empty: true},{empty: true},{empty: false},{empty: false},{empty: false}],
+        [{empty: true},{empty: false},{empty: false},{empty: true},{empty: true},{empty: true},{empty: true},{empty: false},{empty: false},{empty: false}],
+      ],
+      inCart: false,
+      count: 0,
+      price: 10,
+      date: ['05,01,2021', '20:00']
+    }, 
+    {
+      company: "Октябрь",
+      title: "Шанхайские Рыцари",
+      img: "https://i.imgur.com/mgfqO0E.jpg",
+      description: 
+        `Предатель убивает отца Чона Ванга и убегает в Англию. Чон и Рой О’Баннон отправляются в Лондон, 
+        чтобы отомстить злодею. Одновременно сестра Чона Лин раскрывает заговор против королевской семьи, 
+        но ей почти никто не верит. Чону же нужно решить тяжёлые задачи: как отомстить за смерть отца и 
+        удержать склонного к флирту Роя подальше от сестры.`,     
+      seats: [
+        [{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: true},{empty: true},{empty: false},{empty: false},{empty: false}],
+        [{empty: false},{empty: true},{empty: true},{empty: false},{empty: true},{empty: true},{empty: false},{empty: false},{empty: false},{empty: false}],
+        [{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false}],
+        [{empty: false},{empty: false},{empty: false},{empty: true},{empty: true},{empty: true},{empty: false},{empty: false},{empty: false},{empty: false}],
+        [{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: true},{empty: true},{empty: false}],
+        [{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false}],
+        [{empty: true},{empty: true},{empty: true},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false}],
+        [{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false}],
+        [{empty: true},{empty: false},{empty: true},{empty: true},{empty: true},{empty: true},{empty: true},{empty: false},{empty: false},{empty: false}],
+        [{empty: true},{empty: false},{empty: false},{empty: true},{empty: true},{empty: true},{empty: true},{empty: false},{empty: false},{empty: false}],
+      ],
+      inCart: false,
+      count: 0,
+      price: 10,
+      date: ['05,01,2021', '20:00']
+    }, 
+  ],
+  "Аврора": [
+    {
+      company: "Аврора",
+      title: "Шрек",
+      img: "https://i.imgur.com/6sCUZWq.jpg",
+      description: 
+        `Шрек – добродушный зелёный огр, который живёт на болотах. Он считает территорию болот своей и любит уединение. Но в один прекрасный день на болота вдруг заявляются самые разные сказочные персонажи. 
+        Они захватывают дом главного героя мультфильма и заявляют, что теперь будут жить в непосредственном соседстве с ним, 
+        ведь им некуда идти: на болота их сослал правитель сказочного королевства, низкорослый и коварный лорд Фаркуад.
+        Решив разобраться в происходящем, Шрек отправляется к Фаркуаду. По пути он знакомится с ещё одним сказочным персонажем – говорящим Ослом, который, несмотря на свою надоедливость, вскоре становится другом Шрека. 
+        Вместе они прибывают к Фаркуаду, и Шрек договаривается с ним о сделке: он изгонит всех сказочных персонажей с его болот, если Шрек за это спасёт прекрасную принцессу Фиону из лап огнедышащего дракона.
+        Согласившись на сделку, герой мультфильма отправляется в путь. Ему удаётся победить дракона, хотя до этого это не удавалось ни одному рыцарю, 
+        и вызволить принцессу из высокой башни. Теперь их ожидает обратный путь и совместные приключения, 
+        в ходе которых Шрек и сам не замечает, как влюбляется в Фиону. Но он и не догадывается, 
+        что у Фионы тоже есть свой секрет…`,     
+      seats: [
+        [{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: true},{empty: true},{empty: false},{empty: false},{empty: false}],
+        [{empty: false},{empty: true},{empty: true},{empty: false},{empty: true},{empty: true},{empty: false},{empty: false},{empty: false},{empty: false}],
+        [{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false}],
+        [{empty: false},{empty: false},{empty: false},{empty: true},{empty: true},{empty: true},{empty: false},{empty: false},{empty: false},{empty: false}],
+        [{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: true},{empty: true},{empty: false}],
+        [{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false}],
+        [{empty: true},{empty: true},{empty: true},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false}],
+        [{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false},{empty: false}],
+        [{empty: true},{empty: false},{empty: true},{empty: true},{empty: true},{empty: true},{empty: true},{empty: false},{empty: false},{empty: false}],
+        [{empty: true},{empty: false},{empty: false},{empty: true},{empty: true},{empty: true},{empty: true},{empty: false},{empty: false},{empty: false}],
+      ],
+      inCart: false,
+      count: 0,
+      price: 12,
+      date: ['06,01,2021', '20:00']
+    }
+  ]
+}
